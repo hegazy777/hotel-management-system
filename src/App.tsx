f73c2b221845104ceaee5b5a69104b140514d6a4
HalimMahmoud <halimmahmoud50@yahoo.com>
2025-04-15 20:41:37 +0200

    move notfound and landingpage to shared as code review

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import ForgetPassword from "./modules/Authentcations/ForgetPassword/ForgetPasswo
 import ChangePassword from "./modules/Authentcations/ChangePassword/ChangePassword";
 import VerifyAccount from "./modules/Authentcations/VerifyAccount/VerifyAccount";
 import Register from "./modules/Authentcations/Register/Register";
-import NotFound from "./modules/NotFound/NotFound";
 import Login from "./modules/Authentcations/Login/Login";
 import AuthProvider from "./contexts/AuthContext";
 import ProtectedRoute from "./modules/Shared/ProtectedRoute/ProtectedRoute";
 import SnackbarProvider from "./contexts/SnackbarContext";
 import AdminLayout from "./modules/Shared/AdminLayout/AdminLayout";
-import LandingPage from "./modules/LandingPage/LandingPage";
 import AuthLayer from "./modules/Shared/AuthLayout/AuthLayout";
 import Dashboard from "./modules/Dashboard/Dashboard";
+import LandingPage from "./modules/Shared/LandingPage/LandingPage";
+import NotFound from "./modules/Shared/NotFound/NotFound";
 
 function App() {
   const router = createBrowserRouter([
